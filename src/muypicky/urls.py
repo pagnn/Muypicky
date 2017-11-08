43ea5a165f735fe26be6c5162a7fc8dcf801fd2e
Sylvia <sylviawei19950920@gmail.com>
2017-11-08 16:29:52 +0800

    add templates

--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from restaurants.views import home
+from django.views.generic import TemplateView
+from restaurants.views import (
+	restaurants_listview,
+	RestaurantListView,
+	RestaurantDetailView
+	)
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',home),
+    url(r'^$',TemplateView.as_view(template_name='home.html')),
+    url(r'^restaurants/$',restaurants_listview),
+    url(r'^restaurants/(?P<pk>\w+)/$',RestaurantDetailView.as_view()),
+    url(r'^about/$',TemplateView.as_view(template_name='about.html')),
+    url(r'^contact/$',TemplateView.as_view(template_name='contact.html')),
 ]
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,13 +1,29 @@
-import random
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView,ListView,DetailView
+from .models import RestaurantLocation
 
-# Create your views here.
-#function based on view
-def home(request):
-	num=random.randint(1,1000000)
+def restaurants_listview(request):
+	template_name='restaurants/restaurantlocation_list.html'
+	queryset=RestaurantLocation.objects.all()
 	context={
-		"bool_item":True,
-		"num":num
+		'queryset':queryset
 	}
-	return render(request,'base.html',context)#response
+	return render(request,template_name,context)
+
+class RestaurantListView(ListView):
+	def get_queryset(self):
+		slug=self.kwargs.get("slug")
+		if slug:
+			queryset=RestaurantLocation.objects.filter(
+				Q(category__iexact=slug)|
+				Q(category__icontains=slug)
+				)
+		else:
+			queryset=RestaurantLocation.objects.all()
+		return queryset
+class RestaurantDetailView(DetailView):
+	queryset=RestaurantLocation.objects.all()
+
